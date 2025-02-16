6e21d9e197efd834ddc5b8bdd3599ae875ecc907
MatheusZamo <19matheushenrique03@gmail.com>
2025-02-16 15:49:37 -0300

    DarkMode component created

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,5 +1,15 @@
-const DarkMode = () => {
-  return <h1>DarkMode</h1>
+const DarkMode = ({ theme, setTheme }) => {
+  return (
+    <div className="relative">
+      <img
+        src={
+          theme === "light" ? "light-mode-button.png" : "dark-mode-button.png"
+        }
+        className="w-12 cursor-pointer transition-all duration-300"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      />
+    </div>
+  )
 }
 
 export { DarkMode }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { IoMdSearch } from "react-icons/io"
 import { FaCartShopping } from "react-icons/fa6"
 import { DarkMode } from "./DarkMode"
@@ -10,8 +11,14 @@ const menuLinks = [
 ]
 
 const Navbar = () => {
+  const [theme, setTheme] = useState("light")
+
   return (
-    <div className="bg-white duration-200 relative z-40">
+    <div
+      className={`duration-500 relative z-40 ${
+        theme === "dark" ? "bg-gray-900 dark:text-white" : "bg-white"
+      }`}
+    >
       <div className="py-4">
         <div className="p-[1rem] smp-[3rem] flex justify-between items-center">
           <div className="flex items-center gap-4">
@@ -27,7 +34,11 @@ const Navbar = () => {
                   <li key={link.id}>
                     <a
                       href={link.link}
-                      className="inline-block px-4 font-semibold text-gray-500 hover:text-black duration-400"
+                      className={`inline-block px-4 font-semibold text-gray-500 duration-400 ${
+                        theme === "dark"
+                          ? "hover:text-white"
+                          : "hover:text-black"
+                      }`}
                     >
                       {link.name}
                     </a>
@@ -44,14 +55,19 @@ const Navbar = () => {
                 className="search-bar cursor-pointer"
               />
               <IoMdSearch
-                className="text xl text-gray-600 
-              absolute top-1/2 -translate-y-1/2 right-3
+                className={`text xl absolute top-1/2 -translate-y-1/2 right-3
               group-hover:text-[var(--primary)] 
-              duration-200 cursor-pointer"
+              duration-200 cursor-pointer ${
+                theme === "dark" ? "text-gray-400" : "text-gray-600 "
+              }`}
               />
             </div>
             <button className="relative p-3">
-              <FaCartShopping className="text-xl text-gray-600 cursor-pointer" />
+              <FaCartShopping
+                className={`text-xl cursor-pointer ${
+                  theme === "dark" ? "text-gray-400" : "text-gray-600"
+                }`}
+              />
               <div
                 className="w-4 h-4 bg-red-500 text-white
             rounded-full absolute top-0 right-0 flex
@@ -62,7 +78,7 @@ const Navbar = () => {
               </div>
             </button>
             <div>
-              <DarkMode />
+              <DarkMode theme={theme} setTheme={setTheme} />
             </div>
           </div>
         </div>
