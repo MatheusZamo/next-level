e0c616a98ae72bb928e9bfe1e187335bbe5da354
MatheusZamo <19matheushenrique03@gmail.com>
2025-03-04 19:06:32 -0300

    Services component created

--- src/app.jsx
+++ src/app.jsx
@@ -3,16 +3,21 @@ import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/Hero"
 import { Category } from "./components/Category"
 import { Category2 } from "./components/Category2"
+import { Services } from "./components/Services"
 
 const App = () => {
   const [theme, setTheme] = useState("light")
   return (
-    <>
+    <div
+      className={`duration-200 overflow-hidden
+      ${theme === "light" ? "bg-white" : "bg-gray-900 text-white"}`}
+    >
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme} />
       <Category />
       <Category2 />
-    </>
+      <Services />
+    </div>
   )
 }
 
