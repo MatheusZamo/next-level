285a845f5de601f5e67eda3fd77b0eb80506f007
MatheusZamo <19matheushenrique03@gmail.com>
2025-03-05 17:00:05 -0300

    ProductCard component created

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,12 @@
 import { Heading } from "./Heading"
+import { ProductCard } from "./ProductCard"
 
 const Products = () => {
   return (
     <div>
       <div className="p-[1rem] smp-[3rem]">
         <Heading title={"Our Products"} subtitle={"Explore Our Products"} />
+        <ProductCard />
       </div>
     </div>
   )
